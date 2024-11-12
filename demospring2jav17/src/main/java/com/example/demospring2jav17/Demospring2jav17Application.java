9ac2e6d5bb0e1fafc3f399a462b84bdf056617b8
MarkusHWeisse <147412030+MarkusHWeisse@users.noreply.github.com>
2024-11-12 14:30:12 +0100

    setup local development environment task

---
+++ demospring2jav17/src/main/java/com/example/demospring2jav17/Demospring2jav17Application.java
@@ -0,0 +1,13 @@
+package com.example.demospring2jav17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospring2jav17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospring2jav17Application.class, args);
+	}
+
+}
---
+++ demospring2jav17/src/test/java/com/example/demospring2jav17/Demospring2jav17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospring2jav17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demospring2jav17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
